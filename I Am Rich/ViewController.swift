627965ca0629777d93ae14afa74c8106ae5c4721
Jantsen Tanner <jantsentanner@gmail.com>
2021-01-25 20:37:10 -0700

    initial commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Jantsen Tanner on 3/27/20.
+//  Copyright © 2020 Jantsen Tanner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
